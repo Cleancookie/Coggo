5169da16aedd6ae41b97fa63bf136582db0609b7
Alex Law <pokemon999101@hotmail.co.uk>
2018-10-21 01:01:05 +0100

    THE PURGE

--- app/controllers/SocketsController.js
+++
@@ -1,7 +0,0 @@
-exports.init = function(io){
-	io.on('connection', function (socket) {
-		socket.on('yerd', function(){
-			socket.emit('yerded');
-		});	
-	});
-};
--- config/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-    'live' : 'mongodb://localhost/test', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dev' : 'mongodb://localhost/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-     
-     //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
-};
