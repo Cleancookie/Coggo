131e58a138a5f32d0cdc4c13ae8ed255803215e9
Alex Law <pokemon999101@hotmail.co.uk>
2019-08-23 01:03:19 +0100

    🛠 Wrap socketio in koa middleware

--- core/BoneServer.js
+++ core/BoneServer.js
@@ -1,8 +1,7 @@
 const Koa = require('koa');
 const koaEjs = require('koa-ejs');
 const json = require('koa-json');
-const Http = require('http');
-const socketio = require('socket.io');
+const IO = require('koa-socket');
 const BoneRouter = require('./BoneRouter');
 const config = require('../server.config');
 
@@ -12,6 +11,7 @@ const config = require('../server.config');
 class BoneServer {
   constructor() {
     this.app = new Koa();
+    this.io = new IO();
     this.init();
   }
 
@@ -31,11 +31,9 @@ class BoneServer {
    * Initialise websocket functionality
    */
   initWebsockets() {
-    this.http = Http.createServer(this.app.callback());
-    this.io = socketio(this.http);
-
-    this.io.on('connection', socket => {
-      console.log(`User(${socket.id}) has connected`);
+    this.io.attach(this.app);
+    this.app.io.on('connection', ctx => {
+      console.log(`User ${ctx.socket.id} connected`);
     });
   }
 
@@ -67,15 +65,9 @@ class BoneServer {
    * @returns {import('http').Server}
    */
   launchServer(port) {
-    if (config.websockets.enabled) {
-      this.server = this.http.listen(port, () => {
-        console.log(`Listening on port ${port}`);
-      });
-    } else {
-      this.server = this.app.listen(port, () => {
-        console.log(`Listening on port ${port}`);
-      });
-    }
+    this.server = this.app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
 
     return this.server;
   }
